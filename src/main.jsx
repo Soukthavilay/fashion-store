ee0aec0c46793a7503c3cb184c7b319963adad02
Bouphaphan Soukthavilay <58757620+Soukthavilay@users.noreply.github.com>
2024-01-15 22:53:33 +0700

    main:fix create root

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import { BrowserRouter } from "react-router-dom";
-import ReactDOM from 'react-dom';
 import App from './App.jsx';
 import './index.css';
 import './i18n';
 import { LanguageProvider } from './context/LanguageContext';
 import { I18nextProvider } from 'react-i18next';
 import i18n from './i18n';
+import { createRoot } from 'react-dom/client';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
       <I18nextProvider i18n={i18n}>
